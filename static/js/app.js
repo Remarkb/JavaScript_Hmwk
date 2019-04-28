932f0d50bd0afb7a89f88cfb8d511f8fcdb00cfd
Remarkb <BK@BB-MacBook-Pro.local>
2019-04-28 16:29:55 -0500

    fixed filter function by removing original table data before running function for the filter

--- static/js/app.js
+++ static/js/app.js
@@ -5,26 +5,47 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
-console.log(data);
-
+// d3 to build out data in table
 data.forEach(function(ufoReport) {
-    console.log(ufoReport);
+    // console.log(ufoReport);
     var row = tbody.append("tr");
   
     Object.entries(ufoReport).forEach(function([key, value]) {
-      console.log(key, value);
+      // console.log(key, value);
 
-      var cell = tbody.append("td");
+      var cell = row.append("td");
         cell.text(value);
     });
   });
 
+  var submit = d3.select("#filter-btn");
 
+  submit.on("click", function() {
+    d3.event.preventDefault();
 
+    var inputElement = d3.select("#datetime");
 
+    var inputValue = inputElement.property("value");
 
+      console.log(inputValue);
+      console.log(tableData);
+  
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
+      console.log(filteredData); 
+      
+    tbody.html("");
 
-
-
+      filteredData.forEach(function(filteredUfoReport) {
+        console.log(filteredUfoReport);
+        
+        var row = tbody.append("tr");
+      
+        Object.entries(filteredUfoReport).forEach(function([key, value]) {
+          // console.log(key, value);
+    
+        var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+  });
